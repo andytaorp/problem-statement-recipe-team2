d4d2655a9edff3d158e74d99af64e26e605485c5
Athy0202 <23041002@myrp.edu.sg>
2025-02-06 14:12:28 +0800

    Create Nutrient Intake.js

---
+++ frontend/src/pages/Nutrient Intake.js
@@ -0,0 +1,11 @@
+const NutrientIntake = () => {
+  return (
+    <div className="container">
+      <h2>Nutrient Intake</h2>
+      <p>Upload an image to analyze its nutritional content.</p>
+      {/* You can add your ImageUpload and NutritionInfo components here */}
+    </div>
+  );
+};
+
+export default NutrientIntake;
