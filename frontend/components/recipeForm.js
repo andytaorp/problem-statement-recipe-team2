d842deba97a92d501d874dbf16a9586aef35a4a7
fyrdiana <23016424@myrp.edu.sg>
2025-02-06 10:57:22 +0800

    form and env

---
+++ frontend/components/recipeForm.js
@@ -0,0 +1,109 @@
+import { useState } from "react"
+import { useRecipeContext } from '../hooks/useRecipeContext'
+import { useAuthContext } from "../hooks/useAuthContext"
+
+const RecipeForm = () => {
+    const { dispatch } = useRecipeContext()
+    const {user} = useAuthContext()
+
+    const [recipeName, setRecipeName] = useState('')
+    const [ingredients, setIngredients] = useState('')
+    const [instructions, setInstructions] = useState('')
+    const [preparationTime, setPreparationTime] = useState('')
+    const [difficultyLevel, setDifficultyLevel] = useStet('')
+    const [error, setError] = useState(null)
+    const [emptyFields, setEmptyFields] = useState([])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        if (!user) {
+            setError('You must be logged in')
+            return
+        }
+
+        const recipe = {recipeName, ingredients, instructions, preparationTime, difficultyLevel}
+
+        const response = await fetch(`${process.env.REACT_APP_API_URL}/api/recipe`,  {
+            method: 'POST', 
+            body:JSON.stringify(workout),
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${user.token}`
+            }
+        })
+        const json = await response.json()
+
+        if (!response.ok) {
+            setError(json.error)
+            setEmptyFields(json.emptyFields)
+        }
+        if (response.ok) {
+            setRecipeName('')
+            setIngredients('')
+            setInstructions('')
+            setPreparationTime('')
+            setDifficultyLevel('')
+            setError(null)
+            setEmptyFields([])
+            console.log('new recipe added', json)
+            dispatch({type: 'CREATE_RECIPE', payload: json})
+        }
+    }
+
+return(
+    <form className = "create" onSubmit={handleSubmit}>
+        <h3>Add a new workout</h3>
+
+        <label>Recipe name:</label>
+        <input 
+            type = "text" 
+            onChange= {(e) => setRecipeName(e.target.value)}
+            value={recipeName}
+            className={emptyFields.includes('recipeName') ? 'error': ''}
+            />
+
+        <label>Ingredients:</label>
+        <input 
+            type = "text" 
+            onChange= {(e) => setIngredients(e.target.value)}
+            value={ingredients}
+            className={emptyFields.includes('ingredients') ? 'error': ''}
+            />
+
+        <label>Instructions:</label>
+        <input 
+            type = "text" 
+            onChange= {(e) => setInstructions(e.target.value)}
+            value={instructions}
+            className={emptyFields.includes('reps') ? 'error': ''}
+            />
+
+        <label>Preparation time:</label>
+        <input 
+            type = "text" 
+            onChange= {(e) => setPreparationTime(e.target.value)}
+            value={preparationTime}
+            className={emptyFields.includes('preparationTime') ? 'error': ''}
+            />
+
+        <label>Difficulty level:</label>
+        <select
+            onChange={(e) => setDifficultyLevel(e.target.value)}
+            value={difficultyLevel}
+            className={emptyFields.includes('difficultyLevel') ? 'error' : ''}
+            >
+                <option value= ''>Select difficuty</option>
+                <option value='easy'>Easy</option>
+                <option value='medium'>Medium</option>
+                <option value='Hard'>Hard</option>
+            </select>
+
+        <button>Add recipe</button>
+        {error && <div className="error">{error}</div>}
+    </form>
+)
+
+}
+
+export default RecipeForm
